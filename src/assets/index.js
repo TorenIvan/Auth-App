ea14abeb3317a99d96ce8314bbd0631ab447a041
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2021-11-20 13:48:25 +0200

    adding assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+const Assets = {
+  Facebook_icon: require("./images/Facebook.svg"),
+  Github_icon: require("./images/Github.svg"),
+  Google_icon: require("./images/Google.svg"),
+  Twitter_icon: require("./images/Twitter.svg"),
+};
+
+export default Assets;
