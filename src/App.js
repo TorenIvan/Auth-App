980e504b013bcacdb557b1e990b1b8981d99189b
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2022-01-07 16:37:53 +0200

    Auth changed file architecture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes  } from "react-router-dom";
-import Register from "./components/Register/Register";
-import Login from "./components/Login/Login";
+import Register from "./components/Auth/Register/Register";
+import Login from "./components/Auth/Login/Login";
 import "./App.css"
 
 const App = () => {
