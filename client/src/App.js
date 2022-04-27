24cfd6cf782a045ef3a0a913a887b33099ca3aee
Vaggelis <Vaggelisshmos@gmail.com>
2022-04-27 15:27:47 +0300

    Default path on login

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,9 @@ import "./App.css"
 const App = () => {
   return (
     <Routes>
-      <Route exact path={"/register"} element={<Register/>} />
+      <Route exact path={"/"} element={<Login/>} />
       <Route exact path={"/login"} element={<Login/>} />
+      <Route exact path={"/register"} element={<Register/>} />
     </Routes>
   );
 };
