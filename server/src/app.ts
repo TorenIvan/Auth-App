065ba04fb2bd21cd94e25873b1dc19d3e91e59b0
Vaggelis <Vaggelisshmos@gmail.com>
2022-09-17 16:05:39 +0300

    Refactoring basic fastify code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,33 +1,40 @@
 import { join } from "path";
 import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
 import { FastifyPluginAsync } from "fastify";
+import fastifyMongodb from "@fastify/mongodb";
 
 export type AppOptions = {
-	// Place your custom options for app below here.
+  // Place your custom options for app below here.
 } & Partial<AutoloadPluginOptions>;
 
 const app: FastifyPluginAsync<AppOptions> = async (
-	fastify,
-	opts
+  fastify,
+  opts
 ): Promise<void> => {
-	// Place here your custom code!
+  // Place here your custom code!
 
-	// Do not touch the following lines
+  // Do not touch the following lines
 
-	// This loads all plugins defined in plugins
-	// those should be support plugins that are reused
-	// through your application
-	void fastify.register(AutoLoad, {
-		dir: join(__dirname, "plugins"),
-		options: opts,
-	});
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
 
-	// This loads all plugins defined in routes
-	// define your routes in one of these
-	void fastify.register(AutoLoad, {
-		dir: join(__dirname, "routes"),
-		options: opts,
-	});
-};
+  void fastify.register(fastifyMongodb, {
+    forceClose: true,
+    url: process.env.CONNECT_DB,
+  });
+
+//   void fastify.register(AutoLoad, {
+//     dir: join(__dirname, "plugins"),
+//     options: opts,
+//   });
+
+//   // This loads all plugins defined in routes
+//   // define your routes in one of these
+//   void fastify.register(AutoLoad, {
+//     dir: join(__dirname, "routes"),
+//     options: opts,
+//   });
+// };
 
 export default app;
