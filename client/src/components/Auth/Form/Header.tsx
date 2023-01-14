ad6cea2b5e06af83a68542b75ba03cc6414e4b8f
TorenIvan <Vaggelisshmos@gmail.com>
2023-01-14 19:38:20 +0200

    Adding all colors as global variables and use them through theme

--- client/src/components/Auth/Form/Header.tsx
+++ client/src/components/Auth/Form/Header.tsx
@@ -6,7 +6,7 @@ import Constants from "../../../utils/Constants";
 const Header = ({ title }: { title: JSX.Element }): JSX.Element => {
   return (
     <header className={headerStyles.header}>
-      {title}
+      <div>{title}</div>
       <h4>{Constants.FormHeader}</h4>
     </header>
   );
