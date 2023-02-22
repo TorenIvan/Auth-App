89ad3d1cf783d5b4eb128ddd5a2168272ffbc2df
TorenIvan <Vaggelisshmos@gmail.com>
2023-02-22 20:47:55 +0200

    Moving from CRA to Vite react-ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
