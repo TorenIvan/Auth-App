710a6fcef2f6969eb9367ee52132e8f4581aeb04
TorenIvan <Vaggelisshmos@gmail.com>
2023-04-17 17:33:59 +0300

    Initial try on inputs, textarea

--- client/src/routes/Profile/pages/ProfileEdit/ProfileEdit.tsx
+++ client/src/routes/Profile/pages/ProfileEdit/ProfileEdit.tsx
@@ -12,35 +12,49 @@ function ProfileEdit() {
           <h3>Change Info</h3>
           <span>Changes will be reflected to every service</span>
         </article>
-        <section className={styles["edit-item"]}>
-          <figure>
-            <img alt="User Photo with Edit Option" />
-            <figcaption>CHANGE PHOTO</figcaption>
-          </figure>
+        {/* <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <figure>
+              <img alt="User Photo with Edit Option" />
+              <figcaption>CHANGE PHOTO</figcaption>
+            </figure>
+          </section>
+        </section> */}
+        <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <label>Name</label>
+            <input placeholder="Enter your name..."></input>
+          </section>
         </section>
-        <section className={styles["edit-item"]}>
-          <label>Name</label>
-          <input placeholder="Enter your name..."></input>
+        <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <label>Bio</label>
+            <textarea placeholder="Enter your bio..."></textarea>
+          </section>
         </section>
-        <section className={styles["edit-item"]}>
-          <label>Bio</label>
-          <textarea placeholder="Enter your bio..."></textarea>
+        <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <label>Phone</label>
+            <input placeholder="Enter your phone..."></input>
+          </section>
         </section>
-        <section className={styles["edit-item"]}>
-          <label>Phone</label>
-          <input placeholder="Enter your phone..."></input>
+        <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <label>Email</label>
+            <input placeholder="Enter your email..."></input>
+          </section>
         </section>
-        <section className={styles["edit-item"]}>
-          <label>Email</label>
-          <input placeholder="Enter your email..."></input>
+        <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <label>Current Password</label>
+            <input placeholder="Enter your current password..."></input>
+          </section>
         </section>
-        <section className={styles["edit-item"]}>
-          <label>Current Password</label>
-          <input placeholder="Enter your current password..."></input>
-        </section>
-        <section className={styles["edit-item"]}>
-          <label>New Password</label>
-          <input placeholder="Enter your new password..."></input>
+        <section className={styles["edit-item-container"]}>
+          <section className={styles["edit-item"]}>
+            <label>New Password</label>
+            <input placeholder="Enter your new password..."></input>
+          </section>
         </section>
       </fieldset>
     </div>
