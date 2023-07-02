9c5ae5fc9c211493e701bc6d638ace61abfe0b45
TorenIvan <Vaggelisshmos@gmail.com>
2023-07-02 19:06:58 +0300

    Deleting unused component ProfileInput, PasswordInput and Input now that we use GHroupInput everywhere

--- client/src/routes/Profile/components/ProfileInput/index.ts
+++
@@ -1,1 +0,0 @@
-export { ProfileInput as default } from "./ProfileInput";
--- client/src/routes/Profile/components/index.ts
+++ client/src/routes/Profile/components/index.ts
@@ -2,6 +2,5 @@ export { default as UserAvatar } from "./UserAvatar";
 export { default as SideMenu } from "./SideMenu";
 export { default as Divider } from "./Divider";
 export { default as ProfileDetail } from "./ProfileDetail";
-export { default as ProfileInput } from "./ProfileInput";
 export { default as ProfileEditItem } from "./ProfileEditItem";
 export { default as UserPhoto } from "./UserPhoto";
