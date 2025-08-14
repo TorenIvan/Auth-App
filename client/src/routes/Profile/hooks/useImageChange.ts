d85c2dac40b5dba0342ba7482655f7d2eec898eb
TorenIvan <Vaggelisshmos@gmail.com>
2025-08-14 18:00:55 +0300

    Solving the image derived state error on fetching immediately inside Edit screen

--- client/src/routes/Profile/hooks/useImageChange.ts
+++ client/src/routes/Profile/hooks/useImageChange.ts
@@ -1,27 +1,28 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, useEffect } from "react";
 
 function useImageChange(imageName: string | undefined) {
   const [image, setImage] = useState<string | null>(findPhoto(imageName));
+
+  useEffect(() => {
+    setImage(findPhoto(imageName));
+  }, [imageName]);
 
   function handleImageChange(event: ChangeEvent<HTMLInputElement>) {
     event.stopPropagation();
 
     const files = event.currentTarget.files;
-    if (files === null || files === undefined || files.length === 0) {
+    if (!files || files.length === 0) {
       return;
     }
     const imageUrl = URL.createObjectURL(files[0]);
     setImage(imageUrl);
   }
 
-  return [image, handleImageChange] as const;
+  return [image, handleImageChange, setImage] as const;
 }
 
 export { useImageChange };
 
 function findPhoto(image?: string) {
-  if (image === undefined) {
-    return null;
-  }
-  return image;
+  return image ?? null;
 }
