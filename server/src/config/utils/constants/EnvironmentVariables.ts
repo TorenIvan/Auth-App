b57385e9af76eb4051037a31cdcf94a221e12314
TorenIvan <Vaggelisshmos@gmail.com>
2025-11-02 20:25:55 +0200

    Adding new env vars for prod

--- server/src/config/utils/constants/EnvironmentVariables.ts
+++ server/src/config/utils/constants/EnvironmentVariables.ts
@@ -1,11 +1,14 @@
 const encodedDBPassword = encodeURI(String(process.env.DB_PASSWORD));
-const DatabaseUri = `mongodb://${process.env.DB_USERNAME}:${encodedDBPassword}@${process.env.DB_URI}&AuthSource=${process.env.DB_NAME}`;
+const DatabaseUri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_GL_URI!
+    : `mongodb://${process.env.DB_USERNAME}:${encodedDBPassword}@${process.env.DB_URI}&AuthSource=${process.env.DB_NAME}`;
 
 export const EnvironmentVariables = {
   DatabaseUri: DatabaseUri,
   ServerUri: process.env.SERVER_URI!,
   DatabaseName: process.env.DB_NAME,
-  IsProduction: process.env.NODE_END === 'production',
+  IsProduction: process.env.NODE_ENV === 'production',
   Access_Token_Secret: process.env.ACCESS_TOKEN_SECRET!,
   Refresh_Token_Secret: process.env.REFRESH_TOKEN_SECRET!,
   Access_Token_Expiration_Time: process.env.ACCESS_TOKEN_SECRET_EXPIRATION!,
